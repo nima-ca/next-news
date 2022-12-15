15edd24776d018fde879cb9ea553b9f7bddc7d7c
nima <nekoueinima@gmail.com>
2022-12-15 10:56:45 +0330

    [Feat]: add scss and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
-import "../styles/globals.css";
+import "../styles/main.scss";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import { appWithTranslation } from "next-i18next";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default appWithTranslation(App);
